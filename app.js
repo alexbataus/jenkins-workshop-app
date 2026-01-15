88629d8501a0da6502902a05908128795bb7cf2f
alexbataus <abataus@evolution.com>
2026-01-15 15:45:52 +0000

    fix: Implement new endpoint

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ const express = require('express');
       res.send('Hello, CI/CD World!');
     });
 
+ // Add this before module.exports in app.js
+    app.get('/new', (req, res) => {
+      res.send('Hello, New Endpoint!');
+    });
+
     module.exports = app;
